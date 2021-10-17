296946c8b4051f87a747964dcaedd5cdb85c71e4
alansantosmg <alansantosmg@hotmail.com>
2021-10-17 15:31:37 -0300

    Added install project for project 14

--- 14 Distance Converter/Form1.cs
+++ 14 Distance Converter/Form1.cs
@@ -1,28 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace _14_Distance_Converter
 {
-    public partial class Form1 : Form
+  public partial class Form1 : Form
+  {
+    public Form1()
     {
-        public Form1()
-        {
-            InitializeComponent();
-        }
+      InitializeComponent();
+    }
 
-        // close app
-        private void button3_Click(object sender, EventArgs e)
-        {
-          this.Close();
-        }
-      
+    // close app
+    private void button3_Click(object sender, EventArgs e)
+    {
+      this.Close();
+    }
+
     private void ConvertButton_Click(object sender, EventArgs e)
     {
 
@@ -96,8 +89,8 @@ namespace _14_Distance_Converter
       catch
       {
         MessageBox.Show("Selecione as unidades para conversão.");
-        
-      }     
+
+      }
 
     }
     // clear the form
