b145a5dea46a5cf35043c885ff39792c8a8fa616
alansantosmg <alansantosmg@hotmail.com>
2021-11-02 11:18:44 -0300

    Interface completed

---
+++ 21 Km to miles/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace _21_Km_to_miles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.outputListBox = new System.Windows.Forms.ListBox();
+            this.displayButton = new System.Windows.Forms.Button();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // outputListBox
+            // 
+            this.outputListBox.FormattingEnabled = true;
+            this.outputListBox.Location = new System.Drawing.Point(53, 26);
+            this.outputListBox.Name = "outputListBox";
+            this.outputListBox.Size = new System.Drawing.Size(241, 160);
+            this.outputListBox.TabIndex = 2;
+            this.outputListBox.TabStop = false;
+            // 
+            // displayButton
+            // 
+            this.displayButton.BackColor = System.Drawing.SystemColors.Control;
+            this.displayButton.Location = new System.Drawing.Point(53, 211);
+            this.displayButton.Name = "displayButton";
+            this.displayButton.Size = new System.Drawing.Size(75, 41);
+            this.displayButton.TabIndex = 1;
+            this.displayButton.Text = "&Display speeds";
+            this.displayButton.UseVisualStyleBackColor = false;
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.Location = new System.Drawing.Point(134, 210);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(75, 42);
+            this.ClearButton.TabIndex = 2;
+            this.ClearButton.Text = "Cl&ear";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(219, 211);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 42);
+            this.exitButton.TabIndex = 3;
+            this.exitButton.Text = "E&xit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(352, 286);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.ClearButton);
+            this.Controls.Add(this.displayButton);
+            this.Controls.Add(this.outputListBox);
+            this.Name = "Form1";
+            this.Text = "Speed converter";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox outputListBox;
+        private System.Windows.Forms.Button displayButton;
+        private System.Windows.Forms.Button ClearButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
