a98af021622aeb8bfcfd1d7521f95317a523ba84
alansantosmg <alansantosmg@hotmail.com>
2021-11-13 13:46:30 -0300

    interface created for coin flip. Added button clicks methods for face's and tail's coin.

---
+++ 24 Flip Coin/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace _24_Flip_Coin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.facePromptPictureBox = new System.Windows.Forms.PictureBox();
+            this.tailPromptPictureBox = new System.Windows.Forms.PictureBox();
+            this.promptLabel = new System.Windows.Forms.Label();
+            this.faceResultPictureBox = new System.Windows.Forms.PictureBox();
+            this.tailResultPictureBox = new System.Windows.Forms.PictureBox();
+            this.questionResultLabel = new System.Windows.Forms.Label();
+            this.playButton = new System.Windows.Forms.Button();
+            this.facePromptBWPictureBox = new System.Windows.Forms.PictureBox();
+            this.tailPromptBWPictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.facePromptPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tailPromptPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.faceResultPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tailResultPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.facePromptBWPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tailPromptBWPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // facePromptPictureBox
+            // 
+            this.facePromptPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("facePromptPictureBox.Image")));
+            this.facePromptPictureBox.Location = new System.Drawing.Point(47, 69);
+            this.facePromptPictureBox.Name = "facePromptPictureBox";
+            this.facePromptPictureBox.Size = new System.Drawing.Size(164, 148);
+            this.facePromptPictureBox.TabIndex = 0;
+            this.facePromptPictureBox.TabStop = false;
+            this.facePromptPictureBox.Click += new System.EventHandler(this.facePromptPictureBox_Click);
+            // 
+            // tailPromptPictureBox
+            // 
+            this.tailPromptPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("tailPromptPictureBox.Image")));
+            this.tailPromptPictureBox.Location = new System.Drawing.Point(231, 69);
+            this.tailPromptPictureBox.Name = "tailPromptPictureBox";
+            this.tailPromptPictureBox.Size = new System.Drawing.Size(166, 148);
+            this.tailPromptPictureBox.TabIndex = 1;
+            this.tailPromptPictureBox.TabStop = false;
+            this.tailPromptPictureBox.Click += new System.EventHandler(this.tailPromptPictureBox2_Click);
+            // 
+            // promptLabel
+            // 
+            this.promptLabel.AutoSize = true;
+            this.promptLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.promptLabel.Location = new System.Drawing.Point(140, 26);
+            this.promptLabel.Name = "promptLabel";
+            this.promptLabel.Size = new System.Drawing.Size(171, 20);
+            this.promptLabel.TabIndex = 2;
+            this.promptLabel.Text = "Escolha cara ou coroa:";
+            // 
+            // faceResultPictureBox
+            // 
+            this.faceResultPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("faceResultPictureBox.Image")));
+            this.faceResultPictureBox.Location = new System.Drawing.Point(559, 69);
+            this.faceResultPictureBox.Name = "faceResultPictureBox";
+            this.faceResultPictureBox.Size = new System.Drawing.Size(164, 148);
+            this.faceResultPictureBox.TabIndex = 3;
+            this.faceResultPictureBox.TabStop = false;
+            // 
+            // tailResultPictureBox
+            // 
+            this.tailResultPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("tailResultPictureBox.Image")));
+            this.tailResultPictureBox.Location = new System.Drawing.Point(559, 69);
+            this.tailResultPictureBox.Name = "tailResultPictureBox";
+            this.tailResultPictureBox.Size = new System.Drawing.Size(166, 148);
+            this.tailResultPictureBox.TabIndex = 4;
+            this.tailResultPictureBox.TabStop = false;
+            // 
+            // questionResultLabel
+            // 
+            this.questionResultLabel.AutoSize = true;
+            this.questionResultLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 97F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.questionResultLabel.Location = new System.Drawing.Point(577, 69);
+            this.questionResultLabel.Name = "questionResultLabel";
+            this.questionResultLabel.Size = new System.Drawing.Size(134, 148);
+            this.questionResultLabel.TabIndex = 5;
+            this.questionResultLabel.Text = "?";
+            // 
+            // playButton
+            // 
+            this.playButton.Location = new System.Drawing.Point(429, 111);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(102, 71);
+            this.playButton.TabIndex = 8;
+            this.playButton.Text = "Play";
+            this.playButton.UseVisualStyleBackColor = true;
+            // 
+            // facePromptBWPictureBox
+            // 
+            this.facePromptBWPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("facePromptBWPictureBox.Image")));
+            this.facePromptBWPictureBox.Location = new System.Drawing.Point(47, 69);
+            this.facePromptBWPictureBox.Name = "facePromptBWPictureBox";
+            this.facePromptBWPictureBox.Size = new System.Drawing.Size(165, 148);
+            this.facePromptBWPictureBox.TabIndex = 9;
+            this.facePromptBWPictureBox.TabStop = false;
+            this.facePromptBWPictureBox.Click += new System.EventHandler(this.facePromptBWPictureBox_Click);
+            // 
+            // tailPromptBWPictureBox
+            // 
+            this.tailPromptBWPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("tailPromptBWPictureBox.Image")));
+            this.tailPromptBWPictureBox.Location = new System.Drawing.Point(231, 69);
+            this.tailPromptBWPictureBox.Name = "tailPromptBWPictureBox";
+            this.tailPromptBWPictureBox.Size = new System.Drawing.Size(166, 148);
+            this.tailPromptBWPictureBox.TabIndex = 10;
+            this.tailPromptBWPictureBox.TabStop = false;
+            this.tailPromptBWPictureBox.Click += new System.EventHandler(this.tailPromptBWPictureBox_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(768, 270);
+            this.Controls.Add(this.tailPromptBWPictureBox);
+            this.Controls.Add(this.facePromptBWPictureBox);
+            this.Controls.Add(this.playButton);
+            this.Controls.Add(this.questionResultLabel);
+            this.Controls.Add(this.tailResultPictureBox);
+            this.Controls.Add(this.faceResultPictureBox);
+            this.Controls.Add(this.promptLabel);
+            this.Controls.Add(this.tailPromptPictureBox);
+            this.Controls.Add(this.facePromptPictureBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.facePromptPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tailPromptPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.faceResultPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tailResultPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.facePromptBWPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tailPromptBWPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox facePromptPictureBox;
+        private System.Windows.Forms.PictureBox tailPromptPictureBox;
+        private System.Windows.Forms.Label promptLabel;
+        private System.Windows.Forms.PictureBox faceResultPictureBox;
+        private System.Windows.Forms.PictureBox tailResultPictureBox;
+        private System.Windows.Forms.Label questionResultLabel;
+        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.PictureBox facePromptBWPictureBox;
+        private System.Windows.Forms.PictureBox tailPromptBWPictureBox;
+    }
+}
+
---
+++ 24 Flip Coin/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _24_Flip_Coin
+{
+
+
+    
+    public partial class Form1 : Form
+    {
+        private int coinfFliped = 3;
+
+        public Form1()
+        {                      
+            InitializeComponent();
+            faceResultPictureBox.Visible = false;
+            tailResultPictureBox.Visible = false;
+            facePromptBWPictureBox.Visible = false;
+            tailPromptBWPictureBox.Visible = false;
+            
+        }
+
+     
+     
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+
+        private void facePromptPictureBox_Click(object sender, EventArgs e)
+        {
+            facePromptPictureBox.Visible = true;
+            facePromptPictureBox.BorderStyle = BorderStyle.Fixed3D;
+            facePromptBWPictureBox.Visible = false;
+
+            tailPromptPictureBox.Visible = false;
+            tailPromptBWPictureBox.Visible = true;
+            tailPromptBWPictureBox.BorderStyle = BorderStyle.None;
+
+            coinfFliped = 0;
+        }
+
+
+        private void tailPromptPictureBox2_Click(object sender, EventArgs e)
+        {
+
+            tailPromptPictureBox.Visible = true;
+            tailPromptPictureBox.BorderStyle = BorderStyle.Fixed3D;
+            tailPromptBWPictureBox.Visible = false;
+
+
+            facePromptPictureBox.Visible = false;
+            facePromptBWPictureBox.Visible = true;
+            facePromptBWPictureBox.BorderStyle = BorderStyle.None;
+            
+            coinfFliped = 1;
+
+        }
+
+        private void facePromptBWPictureBox_Click(object sender, EventArgs e)
+        {
+            facePromptPictureBox.Visible = true;
+            facePromptPictureBox.BorderStyle = BorderStyle.Fixed3D;
+            facePromptBWPictureBox.Visible = false;
+
+            tailPromptPictureBox.Visible = false;
+            tailPromptBWPictureBox.Visible = true;
+            tailPromptBWPictureBox.BorderStyle = BorderStyle.None;
+
+            coinfFliped = 0;
+
+        }
+
+        private void tailPromptBWPictureBox_Click(object sender, EventArgs e)
+        {
+            tailPromptPictureBox.Visible = true;
+            tailPromptPictureBox.BorderStyle = BorderStyle.Fixed3D;
+            tailPromptBWPictureBox.Visible = false;
+
+
+            facePromptPictureBox.Visible = false;
+            facePromptBWPictureBox.Visible = true;
+            facePromptBWPictureBox.BorderStyle = BorderStyle.None;
+
+            coinfFliped = 1;
+        }
+    }
+}
